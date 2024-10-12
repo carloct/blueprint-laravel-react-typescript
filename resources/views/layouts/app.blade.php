f48bcf31d3dbebb3ae0d9226b86eb085571ccfb3
Carlo <c.catemario@gmail.com>
2024-10-12 12:00:46 +0100

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel</title>
+    @viteReactRefresh
+    @vite('resources/js/app.tsx')
+</head>
+<body>
+    <div id="app">
+        @yield('content')
+    </div>
+</body>
+</html>
+
